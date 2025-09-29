b17430b91736af4765705f1673ed666033045d48
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-09-29 20:11:42 +0100

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Nogartel</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Voltar ao mapa
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
